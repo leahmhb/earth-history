b760b066711112dd1ca57c254d2cb4c152288263
Leah <leah@turtle-farm>
2016-02-03 15:48:36 -0500

    Version 1: labels and years

---
+++ js/validate.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+  var correctCount = 0;
+
+  $("#showBlanks").click(function() {
+    $(".input-group").show();
+  });
+
+  $("#hideBlanks").click(function() {
+    $(".input-group").hide();
+  });
+
+  $("#showAll").click(function() {
+   $(".eon").show();
+   $(".era").show();
+   $(".period").show();
+   $(".epoch").show(); 
+ });
+
+  $("#showEons").click(function() {
+   $(".eon").show(); 
+ });
+
+  $("#showEras").click(function() {
+   $(".era").show();
+ });
+
+  $("#showPeriods").click(function() {
+   $(".period").show();
+ });
+
+  $("#showEpochs").click(function() {
+   $(".epoch").show();
+ });
+
+  $("#hideAll").click(function() {
+   $(".eon").hide();
+   $(".era").hide();
+   $(".period").hide();
+   $(".epoch").hide();
+ });
+
+  $("#hideEons").click(function() {
+   $(".eon").hide();
+ });
+
+  $("#hideEras").click(function() {
+   $(".era").hide();
+ });
+
+  $("#hidePeriods").click(function() {
+   $(".period").hide();
+ });
+
+  $("#hideEpochs").click(function() {
+   $(".epoch").hide();   
+ });
+
+  //Click event to scroll to top
+  $('#scroll').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+
+  $('#geologic-time-period input[type=text]').blur(function(e) {
+    if((this.name) != (this.value)) {          
+      $('#' + this.name).removeClass('has-success').addClass('has-error');  
+
+    } else {
+      $('#' + this.name).removeClass('has-error').addClass('has-success');
+      correctCount++;
+      console.log(correctCount);
+    }    
+  });//end instant validate
+
+
+  if(correctCount == 43){
+    $('#success').show();
+    $('html, body').animate({scrollTop : 0},800);
+  }
+
+
+  $('#reset').click(function(){ 
+    correctCount = 0;
+    $('#success').hide();
+     //restore default
+     $("#geologic-time-period input[type=text]").each(function() {  
+      $('#' + this.name + "_input").removeClass('has-error').removeClass('has-success'); 
+    });
+     $('input[type=text]').val('');//reset all input
+   });//end reset
+
+
+});
+
+
